548c5ca607e6c0135a48f38de0764eff67554c81
Claudine McGowan <claudine.mcgowan@gmail.com>
2020-10-15 21:12:05 +0100

    models, controllers, views working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,15 +8,13 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
-
   def show
     @cocktail = Cocktail.find(params[:id])
-    @dose = Dose.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    if cocktail.save
+    if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
@@ -26,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, )
+    params.require(:cocktail).permit(:name)
   end
 end
 
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,19 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    # link the cocktail in the dose instance to the cocktail instance
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktails/show"
+      render :new
     end
   end
 
